d9d0fe4aabac126da178fa238c84e053895d1487
ImGili <18017158729@163.com>
2022-06-23 14:06:10 +0800

    ✨ feat: swapchain createinfo

--- src/Runtime/Series/h_swap_chain.cpp
+++ src/Runtime/Series/h_swap_chain.cpp
@@ -495,6 +495,15 @@ namespace Series
 					imageCount = swapChainSupport.capabilities.maxImageCount;
 				}
 
+				VkSwapchainCreateInfoKHR createInfo = {};
+				createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+				createInfo.surface = m_Surface;
+				createInfo.minImageCount = imageCount;
+				createInfo.imageFormat = surfaceFormat.format;
+				createInfo.imageColorSpace = surfaceFormat.colorSpace;
+				createInfo.imageExtent = extent;
+				createInfo.imageArrayLayers = 1;
+				createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
 
 			}
 			// **************************** create swap chain ****************************
